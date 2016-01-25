e8995b346ad99c9ad11f2a2e3e9674e53a421310
desaishivam26 <shivamdesaixda@gmail.com>
2016-01-25 15:01:40 +0530

    Its Tesla

--- tesla.mk
+++ tesla.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Tesla stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+PRODUCT_NAME := tesla_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
